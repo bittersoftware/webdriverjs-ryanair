8e74cceb2541c739b56b9fffdf08010dd3cd9666
Thiago <bittersoftware@gmail.com>
2023-11-13 01:59:45 +0100

    update todo description

--- pages/bagsSelection.js
+++ pages/bagsSelection.js
@@ -80,7 +80,8 @@ class BagsSelectionPage extends BasePage {
    * Scroll to the bottom of the page before clicking
    * Although locator is found, sometimes clicking the button has no effect
    * Hard code wait so button can be clicked effectively
-   * TODO: Diag button status to remove hard code sleep
+   * TODO: Implement nav bar PO to detect when the check icon appears so Continue btn is enabled
+   * TODO: This avoid the sleep in this step
    * @returns {undefined}
    */
   async selectContinue() {
