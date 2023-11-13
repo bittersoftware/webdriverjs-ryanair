7205386537bbd48f94c12263d0da25e445e072f4
Thiago <bittersoftware@gmail.com>
2023-11-13 03:23:03 +0100

    update reporter to generate results

--- config/reporter.js
+++ config/reporter.js
@@ -1,32 +1,39 @@
 const { AllureRuntime } = require("allure-js-commons");
 const { CucumberJSAllureFormatter } = require("allure-cucumberjs");
+const path = require("path");
 
 class AllureReporterSettings extends CucumberJSAllureFormatter {
   constructor(options) {
-    super(options, new AllureRuntime({ resultsDir: "./allure-results" }), {
-      labels: [
-        {
-          pattern: [/@feature:(.*)/],
-          name: "epic",
-        },
-        {
-          pattern: [/@severity:(.*)/],
-          name: "severity",
-        },
-      ],
-      links: [
-        {
-          pattern: [/@issue=(.*)/],
-          type: "issue",
-          urlTemplate: "http://localhost:8080/issue/%s",
-        },
-        {
-          pattern: [/@tms=(.*)/],
-          type: "tms",
-          urlTemplate: "http://localhost:8080/tms/%s",
-        },
-      ],
-    });
+    super(
+      options,
+      new AllureRuntime({
+        resultsDir: path.resolve(__dirname, "../allure-results"),
+      }),
+      {
+        labels: [
+          {
+            pattern: [/@feature:(.*)/],
+            name: "epic",
+          },
+          {
+            pattern: [/@severity:(.*)/],
+            name: "severity",
+          },
+        ],
+        links: [
+          {
+            pattern: [/@issue=(.*)/],
+            type: "issue",
+            urlTemplate: "http://localhost:8080/issue/%s",
+          },
+          {
+            pattern: [/@tms=(.*)/],
+            type: "tms",
+            urlTemplate: "http://localhost:8080/tms/%s",
+          },
+        ],
+      }
+    );
   }
 }
 
