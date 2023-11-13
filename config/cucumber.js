3c481447c061b5d83cd53474ff863ff4c5591a39
Thiago <bittersoftware@gmail.com>
2023-11-13 01:50:11 +0100

    remove line for local html reports

--- config/cucumber.js
+++ config/cucumber.js
@@ -1,7 +1,6 @@
 module.exports = {
   default: {
     parallel: 2,
-    format: ["html:cucumber-report.html", "./reporter.js"],
     paths: [".features/*.feature"],
     import: [".utils/*.js", ".pages/*.js"],
   },
